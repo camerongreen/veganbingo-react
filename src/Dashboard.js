f9b161328a13d12f7c0cd793a97aa71f879731ff
Cameron Green <i@camerongreen.org>
2022-06-25 09:26:41 +1000

    Making images responsive

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -74,6 +74,7 @@ export const themeOptions = {
     },
   },
 };
+
 const mdTheme = createTheme(themeOptions);
 
 function DashboardContent(props) {
--- src/Game.js
+++ src/Game.js
@@ -1,6 +1,8 @@
 import * as React from 'react';
+import { useTheme } from '@mui/material/styles';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
+import useMediaQuery from '@mui/material/useMediaQuery';
 
 // Components.
 import Square from './Square';
@@ -10,12 +12,14 @@ import './Game.css';
 
 export default function Game(props) {
   let listItems = [];
+  const theme = useTheme();
   Object.entries(props.data).forEach(([key, value], index) => {
-    listItems.push(<Grid key={key} item xs={3} className={value.colour}><Square key={key} name={key} data={value}/></Grid>);
+    listItems.push(<Grid key={key} item xs={3} className={value.colour}><Square
+      key={key} name={key} data={value}/></Grid>);
   });
   return (
-    <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
-      <Grid container spacing={2}>
+    <Container maxWidth="lg" disableGutters={useMediaQuery(theme.breakpoints.down('sm'))} sx={{ mt: 4, mb: 4 }}>
+      <Grid container spacing={{ xs: 1, sm: 2 }}>
         {listItems}
       </Grid>
     </Container>

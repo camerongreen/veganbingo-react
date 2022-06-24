c8ec01955d04513c85b1d07ddfc38c09911ae55e
Cameron Green <i@camerongreen.org>
2022-06-24 18:23:54 +1000

    About page updated

--- src/About.js
+++ src/About.js
@@ -30,31 +30,23 @@ export default function About() {
             Hi :)
           </p>
           <p>
-            Vegan Bingo was developed for the animals. Originally it was written
-            as an Android application, which you can get that for free from the
-            Play Store.
+            Vegan Bingo was inspired by (what I think is) the original Omnivore Rationalization Bingo by Vegnews. It is made to make light of the things we hear so often as vegans. It's not meant to make fun of anybody, just to make fun.
           </p>
           <p>
-            If you'd like to get in touch about the app or anything else, or
-            just want to read my thoughts, check out my web page at:
-            https://camerongreen.org
+            If you'd like to get in touch about the app or anything else, or just want to read my thoughts, check out my web page at: <a href="https://camerongreen.org" target="_blank">camerongreen.org</a>
           </p>
           <p>
-            You can chat more about Vegan Bingo, post your times, watch tumble
-            weeds roll past, etc at: https://facebook.com/veganbingo
+            You can chat more about Vegan Bingo, post your times, watch tumble weeds roll past (seriously there have been like 2 posts ever), etc at: <a href="https://facebook.com/veganbingo" target="_blank">facebook.com/veganbingo</a>
           </p>
           <p>
-            To read the privacy terms or other FAQs about the app:
-            https://camerongreen.org/a/veganbingo
-            Big thanks to the original Rationalization Bingo in Vegnews which
-            inspired this.
+            To read the privacy terms or other FAQs about the app: <a href="https://camerongreen.org/a/veganbingo" target="_blank">Vegan Bingo on E.A.R.T.H.</a>.
           </p>
           <p>
             This work is licensed under the Creative Commons
             Attribution-NonCommercial-ShareAlike 3.0 Unported License. To view a
-            copy of this license, visit
-            http://creativecommons.org/licenses/by-nc-sa/3.0/.
+            copy of this license, visit <a href="http://creativecommons.org/licenses/by-nc-sa/3.0/" target="_blank">Creative Commons</a>
           </p>
+          <p>Ahimsa!</p>
 
           <IconButton component={Link} to="/">
             <GridOnIcon/>

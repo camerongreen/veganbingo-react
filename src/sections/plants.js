bb1574e94dc5b2668c77150a2e924e4e822f016c
Cameron Green <i@camerongreen.org>
2022-07-03 19:01:04 +1000

    Plants redone

---
+++ src/sections/plants.js
@@ -0,0 +1,31 @@
+// This appears under the pictures on the square, and at the top of its
+// individual page.
+// Format: Text.
+const heading = `Plants have feelings`;
+
+// These are the alternative questions/acceptable statements at the top of the
+// individual page.
+// Format: Text.
+const alternatives = `Carrots scream when you cut them!`;
+
+// This is the short section at the top of the individual page.
+// Format: HTML.
+const short_answer = `<p>Things that don't have a central nervous system don't warrant the same levels of ethical concern as things that do have a central nervous system. If you eat animals, who in turn eat plants, you end up needing a lot more plants to feed yourself. If you are concerned about the suffering of plants, then the best thing to do is to eat more of them.</p>`;
+
+// This is the body of the individual page, use html liberally.
+// Format: HTML.
+const long_answer = `
+<p>Sadly people who say things like this aren't next-level compassionate souls, but are even less likely to care about plants than they do animals.</p>
+<p>So yes, it's a ridiculous question. Most of the animals we eat are herbivores, and eating them means many more plants are used to feed us in the end, than if we just ate plants directly. Also plants don't have a central nervous system, evolutionarily it's not very useful to feel pain if you can't do something in response to it, like run away. Plant biology is many and varied, there is more to be said here, but it would probably lost on anyone silly enough to ask this question.</p>
+<p>Ignoring them, there is a serious point for us to think about here. In evolutionary terms there aren't distinct, easily identifiable separate kingdoms of plants and animals, it is all very much a continuum. We need to ask ourselves where we draw the line and on what basis we do so. People who take animal suffering seriously sometimes think eating simple creatures like oysters and mussels might be OK ethically. Vegans wouldn't do this, but we generally have more respect for these people than those who just draw the line wherever the people around them did, which for most societies in human history once included cannabalism.</p>
+<p>Darwin and the best biologists since have understood that our common evolutionary heritage expresses itself in us sharing much of our sentience with other beings. In understanding how we should treat other beings ethically we should discuss levels of self awareness, the ability to suffer, the presence of a central nervous system, in all of which science should be our guide. This philosophy is called sentientism, that all animals interests deserve consideration. This is opposed to speciesism, which treats all non human life as having lesser worth, not because of their innate capabilities, but purely because they are not human.</p>
+<p>Humans are a destructive force on the animal world, and modern technology has only increased this. The time is well past when we should reconsider the philosophies and ways of living which have led to this reality. Maybe considering the interests of beings other than humans, including plants, is actually something we should take seriously.</p>
+<p>We need to re-evaluate our moral framework, we must rethink very carefully about where we draw our moral lines, of our obligations to the other beings who share our world. In the end if we err in that endeavour let us err on the side of compassion.</p>
+`;
+
+export {
+  heading,
+  alternatives,
+  short_answer,
+  long_answer,
+};

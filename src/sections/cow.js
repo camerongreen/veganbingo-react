5f020ea9a3d065a13b7bc29ece06bc4f413905ba
Cameron Green <i@camerongreen.org>
2022-07-03 18:36:07 +1000

    Adding piggies

---
+++ src/sections/cow.js
@@ -0,0 +1,27 @@
+// This appears under the pictures on the square, and at the top of its
+// Format: Text.
+// individual page.
+const heading = 'What would happen to the cows?';
+
+// These are the alternative questions/acceptable statements at the top of the
+// individual page.
+// Format: Test.
+const alternatives = `What about all the farmers? What about all the nice open fields in the countryside?`;
+
+// This is the short section at the top of the individual page.
+// Format: HTML.
+const short_answer = `<p>Livestock didn't take most of the arable land in the world by themselves, that was us dispossessing the traditional owners, getting rid of the native flora and fauna and then breeding the livestock. We can just stop doing it.</p>`;
+
+// This is the body of the individual page, use html liberally.
+// Format: HTML.
+const long_answer = `
+<p>This is one of those things people say that make you realise just how divorced from the reality of modern industrial farming most people are. The breeding of animals is a business, one strictly controlled by the farmers and companies to maximise profits. Insemination is highly artificial, in fact some animals like turkeys have had their bodies so warped by the selective breeding they are no longer capable of doing so naturally. A life with a body that outgrows its skeletons  and internal organs ability to support itself is no gift, it would be better for them not to live and some less human interfered with creature take up that ecological space. If we are worried about being overrun by freed farm animals, we just need to stop bringing them into the world.</p>
+<p>It is unlikely that a change to a vegan world will happen overnight, so it's not going to be a problem.  Our dairy and meat eating, but somehow animal loving friends, don't need to worry. They can just go ahead and become vegans, without losing sleep over the terrible notion of billions of gentle creatures wandering free across the countryside.</p>
+<p>As for what will happen to the current farmers, butchers, slaughterhouse workers etc, some of the same applies.  They are artificially bred....oh my bad, not that bit, the other bit - change will happen gradually. People will transition to other work and products, just like so many changes in industries and technologies have happened in the past. People still need to eat, the market will adapt, and as is often the case there may be some temporary support required. If fewer people are needed to grow our food, or to spend what aren't great lives killing animals day after day in bloody sheds, then that's probably a good thing. More people can be doing better things to improve our society.</p>
+<p>As for all the nice open fields in the countryside, people may be used to it how it is, but it is actually a desolation. Who and what lived there before? These were teeming, productive ecosystems, integrating millions of years of evolutionary wisdom. At least some of those workers from the meat industry could find more meaningful work rewilding the places we have so changed, removing introduced species and helping native ones return again. Livestock farming, and the grains grown just to feed them, cover a huge percentage of the earth. This is why the UN and other agencies have pointed out that livestock farming is one of the primary causes of species extinction. Not just non humans are threatened by this monopolisation of the land as well, much of the land of dispossessed poor and indigenous people was taken to farm high profit livestock by a privileged elites or colonisers.</p>
+<p>A better world awaits us and the animals, if we can shake off the habits of the past and imagine a better world for us all together.</p>
+`;
+
+export {
+  heading, alternatives, short_answer, long_answer,
+};

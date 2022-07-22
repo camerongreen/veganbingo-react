79122bdda3e167cafc0af0212e12e1fc16fcb51b
Cameron Green <i@camerongreen.org>
2022-07-22 12:08:32 +1000

    Natural

---
+++ src/sections/natural.js
@@ -0,0 +1,30 @@
+// This appears under the pictures on the square, and at the top of its
+// individual page.
+// Format: Text.
+const heading = `Meat eating is natural`;
+
+// These are the alternative questions/acceptable statements at the top of the
+// individual page.
+// Format: Text.
+const alternatives = `Making appeals that it has been done for a long time. Saying killing is part of nature.`;
+
+// This is the short section at the top of the individual page.
+// Format: HTML.
+const short_answer = `<p>Think of all the terrible things that happen in nature, the terrible things humans have done to each other and animals down through the centuries. This argument almost celebrates them, says they should always continue. Whether something is natural, or has been done for a long time, is irrelevant to whether it is necessary, just or compassionate.</p>`;
+
+// This is the body of the individual page, use html liberally.
+// Format: HTML.
+const long_answer = `
+<p>As much as humans wish for a moral ordered universe, the laws which govern the one we live in are amoral. Good things happen to bad people, terrible things happen to good people, all governed by elegant but soulless cosmic mathematical principles. People make up things like karma or heaven and hell to shield themselves from this truth, but ultimately we know it to be true. It would be nice if the universe had some sort of innate justice, but it does not. Natural just simply does not equal right, just or ethical.</p>
+<p>To say then that something is natural and that this is alone needed as a moral defence of the action, would be to ignore the vast number of natural but terrible things that go on. Massive outbreaks of deadly disease used to be common; to intervene with medicine is not purely natural, but surely it would be immoral to do anything else. Cannibalism, slavery, warfare, oppression, all manner of atrocities have long and common histories amongst human society. We could claim they are natural and if so is that some valid ethical defense of them?</p>
+<p>We make advances in society in fields such as law, justice, democracy, modern medicine and philosophy not by appeals to what is natural but what is right. Why would the ethics of the way we treat animals be any different? If we can be healthy eating a diet which doesn't oppress billions of other creatures who we share this earth with, then why wouldn't we?</p>
+<p>A respect for nature is obviously a good and important thing, especially given the limits of our knowledge, and our poor history of intervening in natural systems. But to call the modern diet "natural", or the factory farming system "natural" is to render the word meaningless. People who only eat what they have hunted themselves might be able to make this claim about natural, but that sort of system is never going to feed billions of people meat twice a day. We long ago left living compatibly with the natural world, the destructive animal farming system which covers huge swathes of our planet is one of the first things we need to jettison if we as a species want to reclaim any sort of functional relationship with nature.</p>
+<p>Some things are both natural and good, such as the freedom and joy that every sentient being desires. Taking that from them when you don't need to, when we have such abundance, when you can just choose to eat something different, needs a better justification than just that we are in the habit of it.</p>
+`;
+
+export {
+  heading,
+  alternatives,
+  short_answer,
+  long_answer,
+};

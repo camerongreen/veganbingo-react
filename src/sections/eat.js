218f567761332116e143d4a0807c5e06a0d573b7
Cameron Green <i@camerongreen.org>
2022-08-03 09:54:53 +1000

    More things

---
+++ src/sections/eat.js
@@ -0,0 +1,31 @@
+// This appears under the pictures on the square, and at the top of its
+// individual page.
+// Format: Text.
+const heading = `What can you eat?`;
+
+// These are the alternative questions/acceptable statements at the top of the
+// individual page.
+// Format: Text.
+const alternatives = `Oh you are vegan, that must be hard. Veganism is too expensive.`;
+
+// This is the short section at the top of the individual page.
+// Format: HTML.
+const short_answer = `<p>Anyone who thinks the millions of people who eat a vegan diet are all wasting away to nothing, clearly hasn't met many vegans. Most vegans will tell you they eat a greater variety of food as a vegan than they did before. Vegans come in all shapes and sizes, from athletic, to voluptuous, to slim, to powerful, because being vegan is about reducing cruelty, not pleasure.</p>`;
+
+// This is the body of the individual page, use html liberally.
+// Format: HTML.
+const long_answer = `
+<p>Google tells me there are 20,000 plants that people eat. Well the first result did, the second said 200,000, the next 300,000, the next 80,000, so the exact number varies but it's clearly a lot. There are foods you have never heard of that people eat in other countries, that you would like so much it would make your toes curl. Most people eat a limited subset of foods available in the world, and think there is nothing else. When you tell them you are vegan, they think of their own diet with a handful of things removed, things that are often the psychological centre of their meals. To be fair this is what some vegans initially do, and they often don't thrive because it becomes a diet of lack and self-denial that isn't healthy or as interesting.</p>
+<p>Healthy veganism embraces a different sort of food pyramid. We remove the animal products but don't just stop there, we replace them with a wide variety of other plant-based foods. Anyone eating a healthy diet that most peak nutritional organisations recommend, is probably already eating a diet that is mostly vegan. Vegans then replace the animal product portion of their diet with nuts, seeds, beans, legumes, grains, fungi, tofu, tempeh, seitan. Milk is a good example of the variety we have, instead of just one sort we have soy, almond, macadamia, cashew oat, rice, hemp, quinoa, coconut and more. Everything else you want can be found in a plant based version these days too, plant based cheeses, marshmallows, milk chocolate (a lot of dark choc has always been vegan), condensed milk, mayonnaise, yoghurt, haggis, black pudding, bacon, you get the idea.</p>
+<p>There will be some who identify as junk-food vegans that will roll their eyes at some of the health discussion above and say they went vegan for the animals not their health. They tend to buy the plant based meats and cheeses and want them to look and taste as close to the real thing as possible. This is fine and can make the transition easier, but taken too far it misses out on potential health benefits of being vegan, and make it more expensive than it needs to be.</p>
+<p>Sources of vegan protein are also some of the cheapest things you can feed yourself on, beans, legumes and grains.  Even tofu and tempeh are equivalently priced to animal products. One of healthiest diet's ever studied, what is termed a Wholefoods Plant Based Diet, is also the cheapest diet. Win for your pocket, the animals, and your health.</p>
+<p>The thought of being vegan may seem hard, but most vegans will tell you that once they made the change it became pretty automatic. Most of us eat the same dishes regularly and buy the same general things at the supermarket. Once you find a handful of good regular dishes, you are most of the way there. A good starter list would include a curry, a stir-fry, a bolognese sauce, a burger patty and a couple of good soups such as minestrone or red lentil. Whether you are just starting to cook, or are a foodie, there is an infinite world of vegan blogs, video tutorials, meal plans, shopping tips and recipe suggestions out there.</p>
+<p>A good place to start finding out what vegans eat is to Google your favourite dish followed by "vegan", then get in the kitchen and start experimenting!</p>
+`;
+
+export {
+  heading,
+  alternatives,
+  short_answer,
+  long_answer,
+};
---
+++ src/sections/notmuch.js
@@ -0,0 +1,31 @@
+// This appears under the pictures on the square, and at the top of its
+// individual page.
+// Format: Text.
+const heading = `I don't each much meat`;
+
+// These are the alternative questions/acceptable statements at the top of the
+// individual page.
+// Format: Text.
+const alternatives = `Telling you they ate a vegetarian meal recently.`;
+
+// This is the short section at the top of the individual page.
+// Format: HTML.
+const short_answer = `<p>When someone says "I don't eat much meat", what vegans hear is "I don't eat many animals". It's better than the alternative of eating more animals, but our hearts and thoughts are with the animals in the factory farms and slaughterhouses. So forgive us if we don't seem as happy as people would like about this statement, but it is good that they are trying, it's a positive step and hopefully we'll see them on the vegan side one day.</p>`;
+
+// This is the body of the individual page, use html liberally.
+// Format: HTML.
+const long_answer = `
+<p>This is a positive statement, the only problem is that too often those who say this aren't 100% in touch with reality. People say this that eat meat every day, often for lunch and dinner, sometimes even breakfast. Perhaps it means they eat more chicken and fish than they used to, or perhaps they are having smaller portions. If this is true it is better on many levels, health, environmental, animal suffering, than whatever diet they previously followed, but they are still probably eating lots of animals.</p>
+<p>Not eating a lot of meat doesn't equate to eating less animals. Chickens, some of the most abused animals in the farming system, are tiny compared to a cow or pig. That's why there are so many billions of them living terrible lives in factory farms. The treatment of fish, who range in size but are often small, is exemplified by the fact that they aren't even counted, just measured in tonnes. There is no regard for them as sentient individuals, people still often think that they don't feel pain, which goes against not just science but common sense. Switching your diet from large animals to smaller animals means more animals in factory farms, crushed in nets, suffocated, impaled with hooks or harried into slaughterhouses.</p>
+<p>So the healthy diet, or the more environmentally friendly diet, often comes at the cost of more animals suffering. Which is why vegans don't do a little song and dance when you say that you don't eat much meat to us. There's also the whole dairy industry, which is an environmental and animal suffering disaster.  That's why people who care about animals don't just swap beef for chicken or more cheese, they swap them with beans, tofu, seitan, nuts, seeds, lentils and commercial plant based alternatives.</p>
+<p>Our ideas of what a normal diet is are culturally shaped. Most people predominantly eat the way their parents did, for no better reason than that's what their parents did. The fact that someone eats less meat than their parents, relative or friend, or chooses white meats instead of red meat, does not equate to not eating much meat. Average meat consumption is still very high according to most peak dietary bodies, which is why they consistently encourage people to reduce their meat consumption. Those same peak dietary bodies also point out that you can be just as healthy on vegan diets as any other well planned diet. So there is no need to eat meat in any amount, it is a choice, a choice we can change if we want to tread more lightly on the flora and fauna of our world.</p>
+<p>OK so we want to acknowledge some people really don't eat much meat. If someone has many more days without meat than otherwise, only cook vegan at home, or only eat meat at occasional social gatherings, then they might be able to claim to be not eating much meat. To everyone else saying this, we want to acknowledge that you are making personal progress, but we can't help noticing you still eat a lot of animals, and we aren't quite ready to roll out the red carpet for you yet.</p>
+<p>For the animals who live and die in agribusiness systems before ending up in packaging, are bred to be a product that someone uses with no more thought to it than any other product, for the environment suffering from deforestation, water and air pollution, for the wild animals of the world whose ecosystems and communities are under constant pressure, for the communities at risk of hunger whose grain is instead sent to feed animals for the wealthy of the world, the optimal amount of animals for us to eat will always be zero.</p>
+`;
+
+export {
+  heading,
+  alternatives,
+  short_answer,
+  long_answer,
+};

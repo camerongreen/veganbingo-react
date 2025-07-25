c5f104d8397dfb8c5006719bb0dbd0bf50e7d586
Cameron Green <i@camerongreen.org>
2025-07-25 23:10:57 +1000

    Adding canonical URL

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { createRoot } from 'react-dom/client';
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@mui/material/styles';
+import { HelmetProvider } from '@dr.pogodin/react-helmet';
 import App from './App';
 import theme from './theme';
 
@@ -9,8 +10,10 @@ const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
 root.render(
+  <HelmetProvider>
   <ThemeProvider theme={theme}>
     <CssBaseline/>
     <App/>
-  </ThemeProvider>,
+  </ThemeProvider>
+  </HelmetProvider>,
 );

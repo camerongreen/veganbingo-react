9d439486ac94058c71adda6661b3233486a14b20
Cameron Green <i@camerongreen.org>
2026-01-29 12:18:31 +1000

    Improving code via windsurf

--- src/Score.js
+++ src/Score.js
@@ -1,44 +1,66 @@
-import * as React from 'react';
-import { Link } from 'react-router-dom';
-import { Snackbar } from "@mui/material";
-import MuiAlert from '@mui/material/Alert';
+import React, { useEffect, useState } from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import { Snackbar, Alert } from "@mui/material";
 
 // CSS.
 import './styles/Score.css';
 
-const Alert = React.forwardRef(function Alert(props, ref) {
-  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
-});
+const ScoreDisplay = ({ score, total }) => (
+  <Link to="/timeline/">
+    Bingos: {score}/{total}
+  </Link>
+);
 
-export default function Score(props) {
-  const [open, setOpen] = React.useState(false);
+ScoreDisplay.propTypes = {
+  score: PropTypes.number.isRequired,
+  total: PropTypes.number.isRequired,
+};
+
+const Score = ({ score, total }) => {
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    const handleBingo = (evt) => {
+      evt.stopImmediatePropagation();
+      setOpen(true);
+    };
+
+    document.addEventListener("bingo:add", handleBingo);
+
+    return () => {
+      document.removeEventListener("bingo:add", handleBingo);
+    };
+  }, []);
 
   const handleClose = (event, reason) => {
-    if (reason === 'clickaway') {
+    if (reason === "clickaway") {
       return;
     }
 
     setOpen(false);
   };
 
-  React.useEffect(() => {
-    document.addEventListener('bingo:add', (evt) => {
-      evt.stopImmediatePropagation();
-      setOpen(true);
-    });
-  });
-
   return (
     <div className="Score">
-      <Link to="/timeline/">
-        Bingos: {props.score}/{props.total}
-      </Link>
-      <Snackbar anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
-                open={open} autoHideDuration={1500} onClose={handleClose}>
-        <Alert onClose={handleClose} severity="success" sx={{ width: '100%' }}>
+      <ScoreDisplay score={score} total={total} />
+      <Snackbar
+        anchorOrigin={{ vertical: "top", horizontal: "center" }}
+        open={open}
+        autoHideDuration={1500}
+        onClose={handleClose}
+      >
+        <Alert onClose={handleClose} severity="success" sx={{ width: "100%" }}>
           Bingo! Updating your score
         </Alert>
       </Snackbar>
     </div>
   );
-}
+};
+
+Score.propTypes = {
+  score: PropTypes.number.isRequired,
+  total: PropTypes.number.isRequired,
+};
+
+export default Score;
